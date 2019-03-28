e0e67d790815346c40135d9668ce5b7d6de91154
Jacek Mosakowski <Jacek Mosakowski@DESKTOP-RIS73BT>
2019-03-28 09:02:11 +0100

    Added player names and symbols

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TicTacToe
 {
@@ -10,9 +6,18 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            // Main code for the TicTacToe game
+            string playerNameA = "";
+            string playerNameB = "";
+            char playerSymbolA = 'x';
+            char playerSymbolB = 'o';
+            char[,] board = new char[3, 3];
 
+            Console.Write("Enter the name of player A: ");
+            playerNameA = Console.ReadLine();
+            Console.Write("Enter the name of player B: ");
+            playerNameB = Console.ReadLine();
 
+            // ...
         }
     }
 }
